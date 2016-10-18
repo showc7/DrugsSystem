c1d9901feab144083fbbbf3ada9a4cca10ed8047
Kirill Belov <kirill.belov@itechart-group.com>
2016-10-18 14:19:28 +0300

    configuration for automapper

--- DrugsSystem.WebUI/Global.asax.cs
+++ DrugsSystem.WebUI/Global.asax.cs
@@ -22,19 +22,7 @@ namespace DrugsSystem.WebUI
         protected void InitalizeAutomapper()
         {
             AutoMapper.Mapper.Initialize(cfg => {
-                cfg.CreateMap<Models.ViewModelDrugUnit, DrugSystem.Service.Models.DrugUnitDepotUpdateServiceModel>()
-                    .ForMember(dest => dest.DepotID, opt => opt.MapFrom(c => c.SelectedDepotID));
-                cfg.CreateMap<SelectListItem, SelectListItem>();
-                cfg.CreateMap<DrugsSystem.Models.Depot, SelectListItem>()
-                    .ForMember(dest => dest.Text, opt => opt.MapFrom(c => c.ToString()))
-                    .ForMember(dest => dest.Value, opt => opt.MapFrom(c => c.DepotID.ToString()));
-                cfg.CreateMap<DrugSystem.Service.Models.DrugUnitDepot, Models.ViewModelDrugUnit>()
-                    .ForMember(dest => dest.SelectedDepotID, opt => opt.MapFrom(c => c.Depot.DepotID ))
-                    .ForMember(dest => dest.DrugUnitID, opt => opt.MapFrom(c => c.DrugUnit.DrugUnitID))
-                    .ForMember(dest => dest.DrugUnitPickNumber, opt => opt.MapFrom(c => c.DrugUnit.PickNumber));
-                cfg.CreateMap<DrugsSystem.Models.DrugUnit, SelectListItem>()
-                    .ForMember(dest => dest.Text, opt => opt.MapFrom(c => c.ToString()))
-                    .ForMember(dest => dest.Value, opt => opt.MapFrom(c => c.DrugUnitID));
+                App_Start.AutoMapperWebConfiguration.Configure(cfg);
             });
         }
     }
